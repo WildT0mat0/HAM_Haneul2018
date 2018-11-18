080e5df3e66354ae335151ae98195d80fae0c7be
Dongyeob Han <dyhan724@gmail.com>
2018-11-18 20:21:44 +0900

    Update button_send_new.ino
    1차 수정

--- button_send_new.ino
+++ button_send_new.ino
@@ -3,11 +3,14 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); //LCD 객체 선언, 안되면 당연히 I2C 주소 0x3F로 바꿔보기
 
-int but1 = 5;
-int but2 = 6;
-int but3 = 7; 
-int but4 = 8; //버튼 D5~D8로 설정. 필요에 따라 바꾸면 됨.
-int volt = 9; //혹시 몰라서 전원 D9로 빼놓을라고 설정함. 필요없으면 말고
+int but1 = 40;
+int but2 = 41;
+int but3 = 42; 
+int but4 = 43; //버튼 D40~D43로 설정. 기존 UNO R3에서 MEGA 2560으로 전환함에 따라 핀 변경.
+
+int volt1 = 13; //전원 1번
+int volt2 = 53; //전원 2번
+
 int stat = 0; //상태 변수. 초기값 0, 버튼 눌리면 1로. 응답 없으면 다시 0으로.
 unsigned long prev_time, cur_time = 0; //시간 값. 초기값 0
 char ch = Serial.read(); // 시리얼 통신으로 수신부가 보내는 값
@@ -19,7 +22,8 @@ void setup() {
   pinMode(but2, INPUT);
   pinMode(but3, INPUT);
   pinMode(but4, INPUT);
-  pinMode(volt, OUTPUT);
+  pinMode(volt1, OUTPUT);
+  pinMode(volt2, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -50,6 +54,7 @@ void checkTime(char val){
   Serial.write('x');
   lcd.clear();
   lcd.print("WAIT...");
+  delay(3000);
   Reset();
   }
 }
@@ -85,7 +90,8 @@ void isTeacherHere(int stat, char val){
 }
 
 void loop() {
-  digitalWrite(volt, HIGH);
+  digitalWrite(volt1, HIGH);
+  digitalWrite(volt2, HIGH);
   int but1_val = digitalRead(but1);
   int but2_val = digitalRead(but2);
   int but3_val = digitalRead(but3);
@@ -123,7 +129,11 @@ void loop() {
     prev_time=millis();
     isTeacherHere(stat, 'D');
     } 
-  else{} //아무것도 안눌리거나 여러개 한번에 눌리거나 
+  else{
+    lcd.begin();
+    lcd.backlight();
+    lcd.print("YOU CAN USE IT");
+  } //아무것도 안눌리거나 여러개 한번에 눌리거나 
 
 
 }
