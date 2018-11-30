2e0953cc5da9cdb86c7da369b60f304d2e266d76
Dongyeob Han <dyhan724@gmail.com>
2018-11-30 23:26:56 +0900

    Add files via upload
    시연을 우선시하기 위해 코드 기능 축소 및 간결화.

---
+++ final_recv.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2); //LCD 객체 선언, 안되면 당연히 I2C 주소 0x3F로 바꿔보기
+
+int but = 10; //버튼 D10로 설정. 필요에 따라 바꾸면 됨.
+int volt1 = 9;
+int volt2 = 13;
+
+void setup() {
+  pinMode(but, INPUT);
+  pinMode(volt1, OUTPUT);
+  pinMode(volt2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(volt1, HIGH);
+  digitalWrite(volt2, HIGH);
+  int but_val = digitalRead(but);
+    char ch = Serial.read(); //계속적으로 문자를 읽어들인다
+ 
+  
+      if(ch == 'D') { //'H'를 읽을 경우 LED을 ON 시킨다
+        Serial.print(ch);
+        lcd.begin();
+        lcd.backlight();
+        lcd.print("STUD_CALLING");
+        delay(100);
+        lcd.blink();
+        delay(50); 
+         
+          }
+        if( digitalRead(10) == HIGH){
+          Serial.write('c');
+          lcd.noBacklight();
+          lcd.noBlink();
+          lcd.clear();
+           delay(100);
+          }
+}
